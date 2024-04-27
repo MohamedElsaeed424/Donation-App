b19b33f6571016beb9e491cde8b3c6c21b0a9862
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-04-28 01:58:19 +0300

    Signup is done 👌👌

--- DonationApp/projects/donor/src/app/app.module.ts
+++ DonationApp/projects/donor/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AuthComponent } from './auth/auth.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { DoctorSignupComponent } from './auth/sign-up/doctor-signup/doctor-signup.component';
 import { TeacherSignupComponent } from './auth/sign-up/teacher-signup/teacher-signup.component';
-import { UploadComponent } from './shared/upload/upload.component';
+
 import {ReactiveFormsModule} from "@angular/forms";
 import { GoogleMapComponent } from './shared/google-map/google-map.component';
 import {GoogleMapsModule} from "@angular/google-maps";
+import {NgOptimizedImage} from "@angular/common";
+import { FileUploadComponent } from './shared/file-upload/file-upload.component';
+import { HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -19,15 +23,17 @@ import {GoogleMapsModule} from "@angular/google-maps";
     SignUpComponent,
     DoctorSignupComponent,
     TeacherSignupComponent,
-    UploadComponent,
     GoogleMapComponent,
+    FileUploadComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule ,
-    ReactiveFormsModule,
-    GoogleMapsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        GoogleMapsModule,
+        NgOptimizedImage ,
+      HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
