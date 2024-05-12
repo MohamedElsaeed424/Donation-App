243dcd49870b15caaf079ae0d2594976654ef880
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-05-12 20:42:55 +0300

    Organization dashboard: added airplane animation.

--- DonationApp/projects/organization/src/app/dashboard/dashboard.component.ts
+++ DonationApp/projects/organization/src/app/dashboard/dashboard.component.ts
@@ -9,9 +9,17 @@ import {Organization} from "../auth/Organization.model";
 })
 export class DashboardComponent implements OnInit{
   organization :Organization
+  isAnimating: boolean = false;
   constructor(private authService :AuthService) {
   }
   ngOnInit() {
     this.organization = this.authService.currentOrganization
+  }
+  startAnimation() {
+    this.isAnimating = true;
+    if (this.isAnimating) {
+      // Reset animation when it ends
+      setTimeout(() => this.isAnimating = false, 1300);
+    }
   }
 }
